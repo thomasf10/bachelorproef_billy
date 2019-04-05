2f13ff50624eefdccad6b0aaa195c28b6644e2a6
thomas <thomasfeys10@gmail.com>
2019-04-05 20:42:34 +0200

    skidsteerinh

---
+++ code_Thomas/skidsteering/src/Sensormodule.cpp
@@ -0,0 +1,206 @@
+#include <arduino.h>
+#include "Sensormodule.h"
+#define Kp 5
+#define Ki 2
+#define Kd 2
+#define drempel 500
+bool rechts,links;
+/* sensor module:
+  layout:
+
+            sensor L1 analogepin 0
+            sensor L2: analogepin 1
+            sensor L3: analogepin 2
+
+            sensor R1: analogepin 3
+            sensor R2: analogepin 6
+            sensor R3: analogepin 7
+
+  Waarden: L1 L2 L3 R1 R2 R3 0 0
+*/
+
+Sensormodule::Sensormodule(){
+  }
+
+Sensormodule::Sensormodule(int pinL1,int pinL2, int pinL3, int pinR1, int pinR2, int pinR3){
+  this->pinL1=pinL1;
+  this->pinL2=pinL2;
+  this->pinL3=pinL3;
+  this->pinR1=pinR1;
+  this->pinR2=pinR2;
+  this->pinR3=pinR3;
+  this->waarden=B00000000;
+  this->lasterror=0;
+  this->overtimeerror=0;
+  }
+
+void Sensormodule::update(){
+  //waarde>=drempel => niet op lijn => digitale waarde=0
+  //waarde<drempel => op lijn => digitale waarde=1
+  if(analogRead(pinL1)>=drempel){
+    waarden=waarden&B01111111;
+  }
+  else{
+    waarden=waarden|B10000000;
+  };
+
+  if(analogRead(pinL2)>=drempel){
+    waarden=waarden&B10111111;
+  }
+  else{
+    waarden=waarden|B01000000;
+  };
+
+  if(analogRead(pinL3)>=drempel){
+    waarden=waarden&B11011111;
+  }
+  else{
+    waarden=waarden|B00100000;
+  };
+
+  if(analogRead(pinR1)>=drempel){
+    waarden=waarden&B11101111;
+  }
+  else{
+    waarden=waarden|B00010000;
+  };
+
+  if(analogRead(pinR2)>=drempel){
+    waarden=waarden&B11110111;
+  }
+  else{
+    waarden=waarden|B00001000;
+  };
+
+  if(analogRead(pinR3)>=drempel){
+    waarden=waarden&B11111011;
+  }
+  else{
+    waarden=waarden|B00000100;
+  };
+
+
+}
+void Sensormodule::print_waarden(){
+  int printVal = 0b0000000100000000;
+printVal += (int) waarden;
+  Serial.print("sensorwaarden: ");
+  Serial.println(printVal, BIN);
+}
+
+int Sensormodule::calculatepid(){
+  int error=0;
+  int errorlinks=0;
+  int errorrechts=0;
+//linker sensoren controleren => stuur naar rechts
+  uint8_t linkersensoren=this->waarden&B11100000;
+//TO DO: 11000000,01100000 en eventueel 11100000 toevoegen
+  switch (linkersensoren) {
+    case B10000000:
+      errorlinks=-1;
+      links=true;
+      break;
+    case B11000000:
+      errorlinks=-2;
+      links=true;
+    case B01000000:
+      errorlinks=-3;
+      links=true;
+      break;
+    case B01100000:
+    if(links==1){
+      errorlinks=-4;
+    }
+      break;
+    case B00100000:
+    if(links==1){
+      errorlinks=-5;
+    }
+      break;
+    default:
+      errorlinks=0;
+      links=false;
+      break;
+  }
+
+//rechter sensoren controleren => stuur naar links
+uint8_t rechtersensoren=this->waarden&B00011100;
+//TO DO: 00001100,00011000 en eventueel 00011100 toevoegen
+switch (rechtersensoren) {
+  case B00000100:
+    errorrechts=1;
+    rechts=true;
+    break;
+  case B00001100:
+    errorrechts=2;
+    rechts=true;
+  case B00001000:
+    errorrechts=3;
+    rechts=true;
+    break;
+  case B00011000:
+  if(rechts==1){
+    errorrechts=4;
+  }
+    break;
+  case B00010000:
+  if(rechts==1){
+    errorrechts=5;
+  }
+    break;
+  default:
+    errorrechts=0;
+    rechts=0;
+    break;
+}
+
+error=errorlinks+errorrechts;
+
+  int pidvalue=Kp*error+Ki*overtimeerror+Kd*(error-lasterror);
+  this->overtimeerror+=error;
+  this->lasterror=error;
+
+  return pidvalue;
+}
+
+uint8_t Sensormodule::getwaarden(){
+  return this->waarden;
+}
+ void Sensormodule::updateleds(){
+
+   if(bitRead(this->waarden, 7)==1){
+       digitalWrite(2,HIGH);
+     } else {
+       digitalWrite(2,LOW);
+     }
+   if(bitRead(this->waarden, 6)==1){
+       digitalWrite(7,HIGH);
+     }
+     else {
+       digitalWrite(7,LOW);
+     }
+   if(bitRead(this->waarden, 5)==1){
+       digitalWrite(8,HIGH);
+     }
+     else {
+       digitalWrite(8,LOW);
+     }
+   if(bitRead(this->waarden, 4)==1){
+       digitalWrite(11,HIGH);
+     }
+     else {
+       digitalWrite(11,LOW);
+     }
+     if(bitRead(this->waarden, 3)==1){
+       digitalWrite(12,HIGH);
+     }
+     else {
+       digitalWrite(12,LOW);
+     }
+   if(bitRead(this->waarden, 2)==1){
+       digitalWrite(13,HIGH);
+     }
+     else {
+       digitalWrite(13,LOW);
+     }
+ }
---
+++ code_Thomas/skidsteering/src/main.cpp
@@ -0,0 +1,238 @@
+#include <Arduino.h>
+#include "Led.h"
+#include "Sensormodule.h"
+#include <Wire.h>
+#include "Motorcontrol.h"
+#include <SPI.h>
+#include <MFRC522.h>
+/*aansluiting RFID:
+	SDA:D10
+	SCK:D13
+	MOSI:D11
+	MISO:D12
+	IRQ:unconnected
+	GND:GND
+	3.3V=3.3V
+	RST=D8
+*/
+// pins RFID
+#define SS_PIN 10
+#define RST_PIN 8
+
+//  Two IO EXPANDERS I2C addresses
+#define I2C_ADDRESS_DIR_MOTORS    0x38
+#define I2C_ADDRESS_ADD_PINS      0x39
+
+//  Command Bytes -- Send to control register in the TCA9554
+#define CMD_REG_INPUT   0x00
+#define CMD_REG_OUTPUT  0x01
+#define CMD_REG_POL_INV 0x02
+#define CMD_REG_CONFIG  0x03
+
+//aantal tijd tussen update sensoren in milliseconden
+#define updatetijd 150
+#define motorsnelheid 200 // 200
+#define D 20
+
+
+//objecten declareren
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+Sensormodule module;
+int pidvalue;
+int Vi;
+Motorcontrol motors;
+unsigned long lastmillis;
+unsigned long currentmillis;
+bool rechtdoor;
+int Rg;
+
+void setup(){
+    Rg=0;
+  // RFID pin's (misschien niet nodig testen!)
+    pinMode(11,OUTPUT); //MOSI
+    pinMode(12,OUTPUT); //MISO
+    pinMode(13,OUTPUT); //SCK
+    pinMode(8,OUTPUT); //rst
+
+  // Initiate  SPI bus
+    SPI.begin();
+
+  // Initiate MFRC522
+      mfrc522.PCD_Init();
+
+  // motorsturing
+  motors=Motorcontrol();
+
+  // i2c
+  Wire.begin();
+
+ // Setup Configuration IO expander (Motor Directions)
+ motors.i2C_write_reg(I2C_ADDRESS_DIR_MOTORS, CMD_REG_CONFIG, 0x00);
+
+ // Setup Configuration IO expander (debug led's)
+ motors.i2C_write_reg(I2C_ADDRESS_ADD_PINS, CMD_REG_CONFIG, 0x00);
+
+ // set motor direction: forward
+   motors.i2C_write_reg(I2C_ADDRESS_DIR_MOTORS, CMD_REG_OUTPUT, B10101010);
+   rechtdoor=true;
+
+ // sensormodules
+  module=Sensormodule(0,1,2,3,6,7);
+
+  // seriele monitor
+  Serial.begin(9600);
+
+}
+
+
+void loop(){
+
+  // timing:
+  currentmillis=millis();
+  if(currentmillis>(lastmillis+updatetijd)){
+    lastmillis=currentmillis;
+
+  // update
+    module.update();
+
+  // LED's:
+  motors.i2C_write_reg(I2C_ADDRESS_ADD_PINS, CMD_REG_OUTPUT, module.getwaarden());
+
+  // sturing:
+  Serial.println("sensorwaarden: ");
+  module.print_waarden();
+  pidvalue=module.calculatepid();
+  Serial.println("pid: ");
+  Serial.println(pidvalue);
+  /*
+Vi=Vo * (R-(d/2))/(R+(d/2))
+Vi= snelheid wiel aan binnekant bocht
+Vo= snelheid wiel buitenkant bocht = motorsnelheid
+R= gewenste draaistraal; hoe kleiner R hoe scherper de bocht
+D= afstand tussen de twee wielen
+  */
+  if(pidvalue<0){
+    //stuur naar rechts
+    Rg=-100/pidvalue; // bereken draaistraal
+    Vi=motorsnelheid*(Rg-(D/2))/(Rg+(D/2)); // Bereken snelheid binnenste wielen
+    if(Vi>=0){
+      // wielen draaien in gelijke richting
+      if(rechtdoor==false){
+        motors.i2C_write_reg(I2C_ADDRESS_DIR_MOTORS, CMD_REG_OUTPUT, B10101010);
+        rechtdoor=true;
+      }
+      motors.set_motor_speed(Vi,Vi,motorsnelheid,motorsnelheid);
+    }
+    else{
+      //Vi<0 => wielen draaien in tegengestelde richting
+      motors.i2C_write_reg(I2C_ADDRESS_DIR_MOTORS, CMD_REG_OUTPUT, B01011010);
+      rechtdoor=false;
+      motors.set_motor_speed(-Vi,-Vi,motorsnelheid, motorsnelheid);
+    }
+  }
+  else if(pidvalue>0){
+    // stuur naar links
+    Rg=100/pidvalue; // bereken draaistraal
+    Vi=motorsnelheid*(Rg-(D/2))/(Rg+(D/2)); // Bereken snelheid binnesnste wielen
+    if(Vi>=0){
+      // wielen draaien in gelijke richting
+      if(rechtdoor==false){
+        motors.i2C_write_reg(I2C_ADDRESS_DIR_MOTORS,CMD_REG_OUTPUT,B10101010);
+        rechtdoor=true;
+      }
+      motors.set_motor_speed(motorsnelheid, motorsnelheid, Vi, Vi);
+    }
+    else{
+      //Vi<0 => wielen draaien in tegengestelde richting
+      motors.i2C_write_reg(I2C_ADDRESS_DIR_MOTORS,CMD_REG_OUTPUT,B10100101);
+      rechtdoor=false;
+      motors.set_motor_speed(motorsnelheid,motorsnelheid,-Vi,-Vi);
+    }
+  }
+  else{
+    // PIDvalue=0 => rij rechtdoor
+    if(rechtdoor==false){
+      motors.i2C_write_reg(I2C_ADDRESS_DIR_MOTORS, CMD_REG_OUTPUT, B10101010);
+      rechtdoor=true;
+    }
+    motors.set_motor_speed(motorsnelheid, motorsnelheid, motorsnelheid, motorsnelheid);
+  }
+}
+// RFID loop:
+Serial.println("looking for tag");
+
+// Look for new cards
+if ( ! mfrc522.PICC_IsNewCardPresent())
+{
+  return;
+}
+// Select one of the cards
+if ( ! mfrc522.PICC_ReadCardSerial())
+{
+  return;
+}
+/*
+//Show UID on serial monitor
+Serial.print("UID tag :");
+String content= "";
+byte letter;
+for (byte i = 0; i < mfrc522.uid.size; i++)
+{
+   Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+   Serial.print(mfrc522.uid.uidByte[i], HEX);
+   content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+   content.concat(String(mfrc522.uid.uidByte[i], HEX));
+}
+Serial.println();
+Serial.print("Message : ");
+content.toUpperCase();
+if (content.substring(1) == "E4 11 F9 1F") //change here the UID of the card/cards that you want to give access
+{
+  Serial.println("checkpoint 1");
+  /*to do
+    schrijf naar lcd
+  */
+Serial.println("rfid gevonden");
+
+
+}
+/*to do:
+if(content.substring(1)=="......."){
+Serial.println("checkpoint 2");
+schrijf tijd naar lcd
+}
+*/
+
+
+
+
+
+  /*
+code voor motoren:
+
+void loop(){
+  //set motor speed
+  motors.set_motor_speed(motorsnelheid, motorsnelheid, motorsnelheid, motorsnelheid);
+  /*
+ *  Determining DC motor rotation direction
+ *  Instruction Byte
+ *    2 bits for each motor
+ *    00  or  11  Stop
+ *    01          CCW or CW
+ *    10          CCW or CW
+ *
+ *    Byte: MD MC MB MA
+ */
+ /*
+  motors.i2C_write_reg(I2C_ADDRESS_DIR_MOTORS, CMD_REG_OUTPUT, B01011010); //  Turn right
+   delay(800);
+  motors.i2C_write_reg(I2C_ADDRESS_DIR_MOTORS, CMD_REG_OUTPUT, B00000000); //  Wait
+   delay(500);
+  motors.i2C_write_reg(I2C_ADDRESS_DIR_MOTORS, CMD_REG_OUTPUT, B10101010); //  Move forward
+   delay(300);
+  motors.i2C_write_reg(I2C_ADDRESS_DIR_MOTORS, CMD_REG_OUTPUT, B00000000); //  Wait
+   delay(500);
+}
+
+*/
